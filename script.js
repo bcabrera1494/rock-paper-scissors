8c53e7530c40fa969ed887a1649ee5187cccdf17
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-05 16:09:47 -0700

    Create index.html and script.js files
    Create index.html.
    Code skeleton for index.html.
    Create script.js.
    Link script.js in index.html code.

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let randomNumber = Math.random();
+
+function getComputerChoice() {
+    if (randomNumber <= 0.3333333333333333) {
+        return "Rock";
+        }
+    else if (randomNumber >=0.3333333333333333 <=0.6666666666666666) {
+        return "Paper";
+        }
+    else if (randomNumber >= 0.6666666666666666 ) {
+        return "Scissors"
+        }
+    }
+
+function getHumanChoice() {
+    prompt
+
+}
