0bb37e3e81d80a1a3e322d337ebd06b4acf8889a
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-08 11:43:42 -0700

    Adjust probability for getComputerChoice
    Multiply random number by 3.
    Adjust returns of rock/paper/scissors to reflect rock = 0<1, 1<paper<2, 2<scissors<3

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-const randomNumber = Math.random();
+const randomNumber = (Math.random()*3);
 
 function getComputerChoice() {
-    if (randomNumber <= 0.3333333333333333) {
+    if (randomNumber <= 1) {
         return "Rock";
         }
-    else if (randomNumber >=0.3333333333333333 <=0.6666666666666666) {
+    else if (randomNumber >=1 <=2) {
         return "Paper";
         }
-    else if (randomNumber >= 0.6666666666666666 ) {
+    else if (randomNumber >= 3 ) {
         return "Scissors"
         }
    
